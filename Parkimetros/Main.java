f0042049a833bc4044e1554d05cea903ff21885d
Rafal Daniel Szpecht <rufus111296@gmail.com>
2017-11-01 21:23:36 +0100

    Clean Up

--- Parkimetros/Main.java
+++ Parkimetros/Main.java
@@ -1,63 +1,57 @@
-import java.io.IOException;
 import java.util.*;
 
-    public class Main {
-    // At the moment get TimeLimit exception by the online judge. will be updated
-    
-        static List<Integer> finale = new LinkedList<Integer>();
+public class Main {
 
-        public static void main(String[] args) throws IOException {
-            Scanner s = new Scanner(System.in);
-            String currLine = "";
-            String[] numsLine;
-            String inputs = s.nextLine();
-            int cases = Integer.parseInt(inputs);
-            int[] input;
-            int max;
+    public static List<Integer> finale = new LinkedList<Integer>();
 
-            for (int i = 0; i < cases; i++) {
-                currLine = s.nextLine();
-                numsLine = currLine.split(" ");
+    public static void main(String args[]){
+        int n, max;
+        int[] coins, buf;
+        String[] line1, line2;
+        int numCases = Integer.parseInt(args[0]) * 2;
 
-                max = Integer.parseInt(numsLine[1]);
+        for (int i = 1; i <= numCases; i+=2){
+            finale.clear();
 
+            line1 = args[i].split(" ");
+            line2 = args[i+1].split(" ");
 
-                input = new int[Integer.parseInt(numsLine[0]) + 1];
-                input[0] = 0;
-                for (int k = 1; k < Integer.parseInt(numsLine[0]) + 1; k++){
-                    input[k] = s.nextInt();
-                }
+            n = Integer.parseInt(line1[0]);
+            max = Integer.parseInt(line1[1]);
 
-                s.nextLine();
+            coins = new int[n + 1];
+            coins[0] = 0;
 
-                int[] item = new int[max];
-
-                recursive(input, item, 0);
-
-                // We will print the size of the list with the sums of each possible combination, -1 because the mayor don't want free parkings
-                System.out.println(finale.size() - 1);
-                finale.clear();
+            for (int j = 0; j < line2.length; j++){
+                coins[j] = Integer.parseInt(line2[j]);
             }
-        }
 
-        private static void recursive(int[] input, int[] item, int count){
-            if (count < item.length){
-                for (int i = 0; i < input.length; i++) {
-                    item[count] = input[i];
-                    recursive(input, item, count+1);
-                }
-            }else{
-                condition(item);
-            }
-        }
+            buf = new int[max];
 
-        private static void condition(int[] item){
-            int count = 0;
-            for (int i: item){
-                count += i;
-            }
-            if (!finale.contains(count)){
-                finale.add(count);
-            }
+            recursive(coins, buf, 0);
+
+            System.out.println(finale.size() - 1);
         }
     }
+
+    private static void recursive(int[] input, int[] buf, int count){
+        if (count < buf.length){
+            for (int i = 0; i < input.length; i++) {
+                buf[count] = input[i];
+                recursive(input, buf, count+1);
+            }
+        }else{
+            condition(buf);
+        }
+    }
+
+    private static void condition(int[] item) {
+        int count = 0;
+        for (int i : item) {
+            count += i;
+        }
+        if (!finale.contains(count)) {
+            finale.add(count);
+        }
+    }
+}
