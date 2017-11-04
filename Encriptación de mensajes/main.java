62110a837ce2da0ca1a20270d29a03fb82e6f7fd
Rafal Daniel Szpecht <rufus111296@gmail.com>
2017-11-04 17:27:58 +0100

    Finished Mensaje Codificado

--- Encriptación de mensajes/main.java
+++ Encriptación de mensajes/main.java
@@ -1,25 +1,59 @@
-import java.util.*;
-
 class main{
+
+    static final char JOKER = 'p';
+    static final char[] VOWELS = {'a','e','i','o','u'};
 
     public static void main (String[] args){
         String end = "pFIN";
+        boolean stop = false;
+        String buffer;
         int i = 0;
-        char joker = 'p';
-        char[] vowels = {a,e,i,o,u};
 
-        while(!decoded(args[i]).equals(end)){
-            System.out.println(vowels(decoded(args[i])));
-            i++;
-        }
+        do{
+            buffer = decoded(args[i]);
+            if (!buffer.equals(end)) {
+                System.out.println(vowels(buffer));
+                i++;
+            }else{
+                stop = true;
+            }
+        }while(stop != true);
     }
 
     // This method returns the decrypted message
     private static String decoded(String s){
         int code = code(s);
+        int buff;
+        char c;
         String decoded = "";
 
-        for (int i = 0; i < s.length)
+        for (int i = 0; i < s.length(); i++){
+            c = s.charAt(i);
+            if((c <= 'z') && (c >= 'a')){
+                if (((char)(s.charAt(i) + code) <= 'z') && ((char)(s.charAt(i) +  code) >= 'a')){
+                    decoded += (char)(s.charAt(i) + code);
+                }else if((char)(s.charAt(i) + code) <= 'z'){
+                    buff = (s.charAt(i) - 'a') - 1;
+                    decoded += (char)('z' + (buff + code));
+                }else{
+                    buff = ('z' - s.charAt(i)) - 1;
+                    decoded += (char)('a' + (buff + code));
+                }
+            }else if ((c <= 'Z') && (c >= 'A')){
+                if (((char)(s.charAt(i) + code) <= 'Z') && ((char)(s.charAt(i) +  code) >= 'A')){
+                    decoded += (char)(s.charAt(i) + code);
+                }else if((char)(s.charAt(i) + code) <= 'Z'){
+                    buff = (s.charAt(i) - 'A') - 1;
+                    decoded += (char)('Z' + (buff + code));
+                }else{
+                    buff = ('Z' - s.charAt(i)) - 1;
+                    decoded += (char)('A' + (buff + code));
+                }
+
+            }
+        }
+
+        return decoded;
     }
 
     // This method returns the number of non-accentued vowels in the decrypted message
@@ -39,15 +73,15 @@ class main{
     // This method returns the code for the Cesar encryption algorithm
     private static int code(String s){
         char c = s.charAt(0);
-
+        return JOKER - c;
     }
 
     // This method returns true when a character is a vowel and returns false otherwise
     private static boolean isVowel(char c){
         boolean isIt = false;
-        for (char v: vowels){
+        for (char v: VOWELS){
             if (!isIt){
-                isIt = (v == c)
+                isIt = (v == c);
             }
         }
         return isIt;
